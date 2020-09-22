c56a36cfd0811afc73d67ca47a135fbaf3d8c8b7
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-09-22 20:07:23 +0200

    Espresso testing if all navigation is working correctly

--- app/src/androidTest/java/com/example/android/chessclock/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/android/chessclock/MainActivityTest.kt
@@ -11,7 +11,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-
 class MainActivityTest{
 
     @Rule @JvmField var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
---
+++ app/src/androidTest/java/com/example/android/chessclock/NavigationTest.kt
@@ -0,0 +1,54 @@
+package com.example.android.chessclock
+
+import android.content.Context
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.Espresso.pressBack
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.view.View
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.EnumSet.allOf
+import java.util.regex.Matcher
+
+
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+
+    @Rule @JvmField var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun test_navigation_to_settings() {
+        onView(withId(R.id.settings_button)).perform(click())
+        onView(withId(R.id.settingsLayout)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_backPress_toMainActivity() {
+        onView(withId(R.id.settings_button)).perform(click())
+        onView(withId(R.id.settingsLayout)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.dashBoard)).check(matches(isDisplayed()))
+    }
+
+    // How to get string resources
+    private fun getResourceString(id: Int): String? {
+        val targetContext: Context = InstrumentationRegistry.getTargetContext()
+        return targetContext.getResources().getString(id)
+    }
+
+    // Where to find tab id's
+    @Test
+    fun test_tabs_navigate_toFragments() {
+        onView(withId(R.id.settings_button)).perform(click())
+        onView(withText(getResourceString(R.string.themeStr))).perform(click())
+        onView(withId(R.id.themeLayout)).check(matches(isDisplayed()))
+        onView(withText(getResourceString(R.string.gameStr))).perform(click())
+        onView(withId(R.id.settingsLayoutFrame)).check(matches(isDisplayed()))
+    }
+}
